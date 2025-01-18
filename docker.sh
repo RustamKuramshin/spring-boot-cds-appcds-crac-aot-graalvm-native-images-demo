d3b89a8a12f63b8f656f460b69fb330f2f5b4ad9
RustamKuramshin <leo.in.empty@gmail.com>
2025-01-19 01:04:44 +0300

    commit

--- docker.sh
+++ docker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 docker build -t petclinica .
-
 docker run petclinica:latest
 
 docker build -t petclinica-cds -f DockerfileCDS .
-
 docker run petclinica-cds:latest
+
+docker build -t petclinica-aot-optimizations -f DockerfileAOTOptimizations .
+docker run petclinica-aot-optimizations
