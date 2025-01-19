831a75d4309daee9b3f53f06ee82276a3e15321b
RustamKuramshin <leo.in.empty@gmail.com>
2025-01-19 21:39:21 +0300

    commit

--- docker.sh
+++ docker.sh
@@ -11,3 +11,11 @@ docker run petclinica-aot-optimizations
 
 docker build -t petclinica-native -f DockerfileNative .
 docker run petclinica-native
+
+jcmd spring-petclinic.jar JDK.checkpoint
+docker build --progress=plain -t petclinica-crac-ff -f DockerfileCRaC .
+#docker buildx build --progress=plain --allow security.insecure --load -t petclinica-crac-ff -f DockerfileCRaC .
+docker run -it --cap-add=CHECKPOINT_RESTORE --cap-add=SYS_PTRACE --rm --name petclinica-crac-ff-checkpoint -v $PWD/crac-files:/app/crac-files petclinica-crac-ff java -Dspring.context.checkpoint=onRefresh -XX:CRaCCheckpointTo=/app/crac-files -jar spring-petclinic.jar
+#docker run -it --cap-add=CHECKPOINT_RESTORE --cap-add=SYS_PTRACE --rm --name petclinica-crac-ff -v $PWD/crac-files:/app/crac-files petclinica-crac-ff java -XX:CRaCCheckpointTo=/app/crac-files -jar spring-petclinic.jar
+docker exec petclinica-crac-ff jcmd spring-petclinic.jar JDK.checkpoint
+docker run -it --cap-add=CHECKPOINT_RESTORE --cap-add=SYS_PTRACE --rm --name petclinica-crac-ff-restore -v $PWD/crac-files:/app/crac-files petclinica-crac-ff java -XX:CRaCRestoreFrom=/app/crac-files
---
+++ src/main/java/org/springframework/samples/petclinic/system/CheckpointFailureListener.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.system;
+
+import org.crac.CheckpointException;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckpointFailureListener implements ApplicationListener<ApplicationFailedEvent> {
+
+	@Override
+	public void onApplicationEvent(ApplicationFailedEvent event) {
+		Throwable exception = event.getException();
+		if (exception != null) {
+			if (exception.getCause() instanceof CheckpointException) {
+				System.err.println("CRaC checkpoint creation failed. Terminating application.");
+				System.exit(0);
+			}
+		}
+	}
+}
