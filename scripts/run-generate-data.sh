ef2ace15211bbcc147c851d220d0c46c963f8f3a
RustamKuramshin <leo.in.empty@gmail.com>
2025-01-24 00:15:09 +0300

    commit

--- scripts/run-generate-data.sh
+++ scripts/run-generate-data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-groovy generateData.groovy 10000 petclinic,petclinicaot,petcliniccds,petcliniccrac,petclinicnative
+groovy generateData.groovy 500 petclinic,petclinicaot,petcliniccds,petcliniccrac,petclinicnative
